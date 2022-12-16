b9f0ec053eeec0d20a5bd7d32c671d4da265ffdf
José Vitor <josev4854@gmail.com>
2022-12-16 20:54:26 -0300

    INserindo anotação mais log

--- src/main/java/br/jvprojetos17/userapi/service/ipml/UserServiceImpl.java
+++ src/main/java/br/jvprojetos17/userapi/service/ipml/UserServiceImpl.java
@@ -4,19 +4,26 @@ import br.jvprojetos17.userapi.domain.User;
 import br.jvprojetos17.userapi.exception.ResourceNotFoundException;
 import br.jvprojetos17.userapi.repository.UserRepository;
 import br.jvprojetos17.userapi.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
+@RequiredArgsConstructor
 @Service
 public class UserServiceImpl implements UserService {
 
+    private final Environment env;
     @Autowired
     private UserRepository userRepository;
 
     @Override
     public User findById(Long id) {
+        log.info("PAYROLL_SERVICE ::: Get request on " + env.getProperty("local.server.port"));
         return userRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("User not found for id: " + id));
     }
 
